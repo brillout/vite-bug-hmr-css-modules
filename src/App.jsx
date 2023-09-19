a2f4e1d634f7e37519d535f19c215af41fc067df
Romuald Brillout <git@brillout.com>
2023-09-19 16:24:18 +0200

    reprod

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import styles from './test.module.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className={styles.container}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -27,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
